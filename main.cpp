af86f253f9cf70c54459627235e64f3abcc7b43c
Parkchangjin <brainnia@naver.com>
2021-03-20 08:31:51 +0000

    Test in ipad

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+  /*
   //BMI : 몸부게 / 키(m) * 키(m))
   float weight = 0;
   float height = 0;
@@ -13,4 +14,6 @@ int main() {
   printf("BMI 지수는 %d 입니다! 이 돼지놈아", bmi); 
 
   printf("this is practice");
+  */
+  printf("this is practice on ipad")
 }
